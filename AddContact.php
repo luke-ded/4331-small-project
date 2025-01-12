70cbb9b3ffb0a935d0f5953f98e6039f683be14a
cmcburnieucf <cmknight@ucf.edu>
2025-01-12 18:33:50 -0500

    Add files via upload

---
+++ AddContact.php
@@ -0,0 +1,42 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$userId = $inData["userId"];
+	$fname = $inData["firstName"];
+	$lname = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+
+	$conn = new mysqli("localhost", "UserName", "Password", "DataBase-Name"); //change this if needed
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT INTO Contacts (UserId,FirstName,LastName,Phone,Email) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("issss", $userId, $fname, $lname, $phone, $email);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
