51364b77d4048c6063074841bc8aaac4d4ed9484
Daksh987 <duckcheese23@gmail.com>
2025-01-12 21:06:38 -0500

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,1 +1,16 @@
+create database COP4331;
 
+use COP4331;
+
+
+CREATE TABLE `COP4331`.`Contacts`
+(
+    `ID` INT NOT NULL AUTO_INCREMENT ,
+    `FirstName` VARCHAR(50) NOT NULL DEFAULT '' ,
+    `LastName` VARCHAR(50) NOT NULL DEFAULT '' ,
+    `Phone` VARCHAR(50) NOT NULL DEFAULT '' ,
+    `Email` VARCHAR(50) NOT NULL DEFAULT '' ,
+    `UserID` INT NOT NULL DEFAULT '0' ,
+     PRIMARY KEY (`ID`)
+
+)ENGINE = InnoDB;
