c1a35713c32c0b265b27f58b0ca5c1fe94197e9e
cmcburnieucf <cmknight@ucf.edu>
2025-01-12 22:45:15 -0500

    Update Login.php

--- api/Login.php
+++ api/Login.php
@@ -14,7 +14,7 @@
 	else
 	{
 	    //Find the user in the table Users based on the given Username and Password, where both should be string values
-		$stmt = $conn->prepare("SELECT UserID,FirstName,LastName FROM Users WHERE Username=? AND Password=?");
+		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Username=? AND Password=?");
 		$stmt->bind_param("ss", $inData["username"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
