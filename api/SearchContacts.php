04703144f5ecfa835efd4e38af6fe5d2f661650d
cmcburnieucf <cmknight@ucf.edu>
2025-01-13 14:21:40 -0500

    Update SearchContacts.php

--- api/SearchContacts.php
+++ api/SearchContacts.php
@@ -6,7 +6,7 @@
 	$searchResults = "";
 	$searchCount = 0;
 
-	$conn = new mysqli("localhost", "UserName", "Password", "COP4331"); //change this if needed
+	$conn = new mysqli("localhost", "root", "&&C0P##4331##Pr0ject&&s", "COP4331"); //change this if needed
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
