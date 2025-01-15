4c57270057d60a17768ed4c2b1a6d9056cd446dd
cmcburnieucf <cmknight@ucf.edu>
2025-01-15 18:53:54 -0500

    Update AddContact.php

--- api/AddContact.php
+++ api/AddContact.php
@@ -1,4 +1,7 @@
-<?php
+<?php //AddContact.php
+    
+    include_once 'UtilFunctions.php';
+    
 	$inData = getRequestInfo();
 	
 	$userId = $inData["userId"];
@@ -21,22 +24,6 @@
 		$conn->close();
 		returnWithError("");
 	}
-
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
 	
 ?>
+
