9e52d9e43a5f571a43b5e19201db1e5acc2ab50b
cmcburnieucf <cmknight@ucf.edu>
2025-01-15 19:21:30 -0500

    Update SignUp.php

--- api/SignUp.php
+++ api/SignUp.php
@@ -1,8 +1,10 @@
-<?php
-
+<?php //SignUp.php
+    
+    include_once 'UtilFunctions.php';
+    
 	$inData = getRequestInfo();
 	$username = $inData["loginName"];
-	$password = $inData["loginPassword"];
+	$password = $inData["password"];
 
 	$conn = new mysqli("localhost", "root", "&&C0P##4331##Pr0ject&&s", "COP4331"); //change this if needed
 	if( $conn->connect_error )
@@ -17,10 +19,10 @@
 		$stmt->execute();
 		
 
-		if( $row = $result->fetch_assoc()  )
+		if( $row = $result->fetch_assoc() )
 		{
 		    //if user exists, send an error message
-		    returnWithError("User Already Exists");
+		    returnWithErrorWithId("User Already Exists");
 		}
 		else
 		{
@@ -28,27 +30,11 @@
 		    $stmt = $conn->prepare("INSERST INTO Users (Login,Password) VALUES(?,?)");
 		    $stmt->bind_param("ss", $username, $password);
 		    $stmt->execute();
+		    returnWithError("");
 		}
 
 		$stmt->close();
 		$conn->close();
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
 	}
 	
 ?>
