dc693e0104c80536f0e0f33b4bd9ca780b59b015
cmcburnieucf <cmknight@ucf.edu>
2025-01-15 19:23:11 -0500

    Create UtilFunctions.php

---
+++ api/UtilFunctions.php
@@ -0,0 +1,32 @@
+<?php //UtilFunctions.php
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithErrorWithId( $err )
+	{
+		$retValue = '{"id":0,"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $info )
+	{
+		$retValue = '{'. $info .',"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
