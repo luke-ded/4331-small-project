39efb44fe18c614fe5b2ddbdcc0f2f0da038ae46
cmcburnieucf <cmknight@ucf.edu>
2025-01-22 17:16:24 -0500

    Update Login.php

--- api/Login.php
+++ api/Login.php
@@ -8,7 +8,7 @@
 	
 	$id = 0;
 
-	$conn = new mysqli("localhost", "root", "&&C0P##4331##Pr0ject&&s", "COP4331"); //change this if needed
+	$conn = new mysqli("localhost", "James98", "password", "COP4331"); //change this if needed
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
