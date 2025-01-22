034dbfd2b4f3d23891294124d4aafcdbbff86db6
Daksh987 <duckcheese23@gmail.com>
2025-01-22 12:57:52 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -15,6 +15,17 @@ function doLogin()
 	
 	let login = userName = document.getElementById("loginName").value;
 	let password = document.getElementById("loginPassword").value;
+
+	if ((login.length == 0) && (password.length == 0)) {
+		document.getElementById("loginResult").innerHTML = "Both fields are Empty!";
+		return;
+	} else if ((login.length != 0) && (password.length == 0)) {
+		document.getElementById("loginResult").innerHTML = "Password is Empty!";
+		return;
+	} else if ((login.length == 0) && (password.length != 0)){
+		document.getElementById("loginResult").innerHTML = "Username is Empty!";
+		return;
+	)
 	
 	document.getElementById("loginResult").innerHTML = "";
 
