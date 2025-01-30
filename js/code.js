a8e830e265878ea1c4989a3e1fb9df7c6bf2acf1
Daksh987 <duckcheese23@gmail.com>
2025-01-30 18:32:37 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -311,7 +311,10 @@ function removeContact()
 	{
 		xhr.onreadystatechange = function() 
 		{
-			
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				console.log("contact is deleted");
+			}
 		};
 		xhr.send(jsonPayload);
 
