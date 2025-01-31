b2e89946ca4c03828169b8d86021e8a0edf1da37
Daksh987 <duckcheese23@gmail.com>
2025-01-30 19:42:21 -0500

    Update DeleteContact.php

--- api/DeleteContact.php
+++ api/DeleteContact.php
@@ -3,7 +3,11 @@
     include_once 'UtilFunctions.php';
     
 	$inData = getRequestInfo();
-	
+
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
 	$userId = $inData["userId"];
 	//$contactId = $inData["contactId"];
 
@@ -17,7 +21,7 @@
 		//$stmt = $conn->prepare("DELETE FROM Contacts WHERE UserId=? AND ID=?");
 		//$stmt->bind_param("ii", $userId, $contactId);
 		$stmt = $conn->prepare("DELETE FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ?) AND UserId=?");
-		$stmt->bind_param("ii", $searchColVal, $searchColVal, $searchColVal, $searchColVal, $userId);
+		$stmt->bind_param("ii", $firstName, $lastName, $phone, $email, $userId);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
