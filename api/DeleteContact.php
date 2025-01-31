3c90d59bb2f90d05aaaf552166fb839eb2fd4fac
Daksh987 <duckcheese23@gmail.com>
2025-01-30 19:34:50 -0500

    Update DeleteContact.php

--- api/DeleteContact.php
+++ api/DeleteContact.php
@@ -5,7 +5,7 @@
 	$inData = getRequestInfo();
 	
 	$userId = $inData["userId"];
-	$contactId = $inData["contactId"];
+	//$contactId = $inData["contactId"];
 
 	$conn = new mysqli("localhost", "James98", "password", "COP4331"); //change this if needed
 	if ($conn->connect_error) 
@@ -14,8 +14,10 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("DELETE FROM Contacts WHERE UserId=? AND ID=?");
-		$stmt->bind_param("ii", $userId, $contactId);
+		//$stmt = $conn->prepare("DELETE FROM Contacts WHERE UserId=? AND ID=?");
+		//$stmt->bind_param("ii", $userId, $contactId);
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ?) AND UserId=?");
+		$stmt->bind_param("ii", $searchColVal, $searchColVal, $searchColVal, $searchColVal, $userId);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
