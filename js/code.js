5890fcc3944758bbb4d3678d148073734f300199
Daksh987 <duckcheese23@gmail.com>
2025-02-08 18:01:02 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -509,16 +509,9 @@ function searchContact()
 				
 				if(jsonObject.error == "No Records Found"){
 					
-					document.getElementById("noResultsDiv").classList.remove("hidden");
-					document.getElementById("tableDiv").classList.add("hidden");
+					document.getElementById("SearchTable").innerHTML = "**No Records Found!**";
 					
-				} else{
-					document.getElementById("tableDiv").classList.remove("hidden");
-					document.getElementById("noResultsDiv").classList.add("hidden");
-					
-					
-
-				}
+				} 
 
 				
 				for( let i=0; i<jsonObject.results.length; i++ )
